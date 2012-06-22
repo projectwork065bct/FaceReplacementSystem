fa7daa2452b6f5c0cce237e8a5b7207b2833890b
Robik <>
2012-06-22 09:30:25 +0545

    First commit for the major project

--- FaceReplacementSystem/src/facereplacementsystem/FaceReplacementSystem.java
+++ FaceReplacementSystem/src/facereplacementsystem/FaceReplacementSystem.java
@@ -15,6 +15,6 @@ public class FaceReplacementSystem {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hello world");
+        System.out.println("hello universe");
     }
 }
