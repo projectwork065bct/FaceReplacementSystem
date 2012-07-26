fe0694a63c29a6690969dc36a730c493f420f07f
Robik <>
2012-07-26 19:23:42 +0545

    Data Structures

---
+++ FaceReplacementSystem/src/DataStructures/FloatingCoordinate.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataStructures;
+
+/**
+ *
+ * @author Dell
+ */
+public class FloatingCoordinate {
+    public FloatingCoordinate()
+    {}
+    public float x;
+    public float y;
+}
