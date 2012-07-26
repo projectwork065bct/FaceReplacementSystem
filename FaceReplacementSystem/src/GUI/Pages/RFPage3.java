f2bb5f9a3ca2229fa4cf3db8187518bed026ae3c
Robik <>
2012-07-26 19:24:17 +0545

    It consists of various pages

--- FaceReplacementSystem/src/GUI/Pages/RFPage3.java
+++ FaceReplacementSystem/src/GUI/Pages/RFPage3.java
@@ -2,26 +2,41 @@ package GUI.Pages;
 
 import GUI.Components.RFApplication;
 import GUI.Components.ResizableRectangleView;
+import hu.droidzone.iosui.IOSUIButton;
+import hu.droidzone.iosui.IOSUIView;
 import java.awt.Color;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
 
 public class RFPage3 extends RFPage {
 
-    ResizableRectangleView rrv = new ResizableRectangleView("600px", "400px");
-    
-    public RFPage3(RFApplication app) {
+    IOSUIView mainView = new IOSUIView("500px,100px", "400px");
+    ResizableRectangleView rrv = new ResizableRectangleView("500px", "400px");
+    IOSUIButton exptBtn;//Button for experimentation
+
+    public RFPage3(final RFApplication app) {
         super(app, "Second Page");
-//		IOSUITextArea ta = new IOSUITextArea(new Font("Verdana", Font.BOLD, 22));
-//		ta.setText("Second Page Content");
-//		ta.setForeground(Color.WHITE);
-//		addXY(ta,1,1,"f,f");
         rrv.setFRS(frs);
         rrv.setImage(frs.getSourceImage());
         rrv.initializeRectangle("source");
         rrv.setBackground(Color.red);
-        addXY(rrv, 1, 1, "f,f");
+        mainView.addXY(rrv, 1, 1, "f,f");
+
+        exptBtn = new IOSUIButton(new AbstractAction("Experiment") {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setSourceRectangle();
+                pc.navigateTo(new RFPage3Expt(app));
+            }
+        });
+
+        mainView.addXY(exptBtn, 2, 1);
+        addXY(mainView, 1, 1);
         helpText.setText("Draw a rectangle around the face.");
+
     }
 
     @Override
@@ -29,21 +44,23 @@ public class RFPage3 extends RFPage {
         setSourceRectangle();
         pc.navigateTo(new RFPage4(app));
     }
-    
+
     //Saves the rectangle drawn around the face
-    public void setSourceRectangle()
-    {
-        Rectangle r=rrv.getRectangle();
-        Rectangle resized=new Rectangle();
-        int recX=r.x;
-        int recY=r.y;
-        int width=r.width;
-        int height=r.height;
-        int stopx=recX+width;
-        int stopy=recY+height;
-        Point a=rrv.toActualImagePoint(new Point(recX,recY));
-        Point b=rrv.toActualImagePoint(new Point(stopx,stopy));
-        resized.x=a.x;resized.y=a.y;resized.width=b.x-a.x;resized.height=b.y-a.y;
+    public void setSourceRectangle() {
+        Rectangle r = rrv.getRectangle();
+        Rectangle resized = new Rectangle();
+        int recX = r.x;
+        int recY = r.y;
+        int width = r.width;
+        int height = r.height;
+        int stopx = recX + width;
+        int stopy = recY + height;
+        Point a = rrv.toActualImagePoint(new Point(recX, recY));
+        Point b = rrv.toActualImagePoint(new Point(stopx, stopy));
+        resized.x = a.x;
+        resized.y = a.y;
+        resized.width = b.x - a.x;
+        resized.height = b.y - a.y;
         frs.setSourceFaceRectangle(resized);
     }
 }
--- FaceReplacementSystem/src/GUI/Pages/RFPage6.java
+++ FaceReplacementSystem/src/GUI/Pages/RFPage6.java
@@ -1,19 +1,28 @@
 package GUI.Pages;
 
+import CoreAlgorithms.Shifter;
 import GUI.Components.IOSUIImageView;
 import GUI.Components.RFApplication;
+import Helpers.DeepCopier;
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
 
 public class RFPage6 extends RFPage {
 
     //DrawFinalImage finalResult = new DrawFinalImage("600px", "400px");
-    IOSUIImageView finalResult = new IOSUIImageView("600px", "400px");
+    IOSUIImageView finalResultView = new IOSUIImageView("600px", "400px");
 
     public RFPage6(RFApplication app) {
         super(app, "The face has been replaced!");
-        finalResult.setImage(frs.getWarpedImage());
-        addXY(finalResult, 1, 1, "f,f");
+        //finalResultView.setImage(frs.getWarpedImage());//get replaced face
+        frs.replaceFace();
+        finalResultView.setImage(frs.getReplacedFaceImage());
+        addXY(finalResultView, 1, 1, "f,f");
         helpText.setText("Jump and dance!");
     }
+
+    
 
     @Override
     public void goNext() {
